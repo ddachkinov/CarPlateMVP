3c6239c2182d3de10618be80675291d88999f36a
ddachkinov <ddachkinov@gmail.com>
2025-04-26 19:19:46 +0300

    switch to render

--- client/src/api/plates.js
+++ client/src/api/plates.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'http://localhost:5001/api' });
-const API = axios.create({ baseURL: 'https://your-backend-name.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://carplatemvp.onrender.com/api' });
 
 
 export const getPlates = () => API.get('/plates');
