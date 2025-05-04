abaa75481359acdcf04e2d711ad392ed4177a3c9
ddachkinov <ddachkinov@gmail.com>
2025-05-04 19:22:35 +0300

    switch to remote backend

--- client/src/api/plates.js
+++ client/src/api/plates.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 // Local for development
-const API = axios.create({ baseURL: 'http://localhost:5001/api' });
+// const API = axios.create({ baseURL: 'http://localhost:5001/api' });
 
 // Remote for deployment (uncomment when pushing)
-// const API = axios.create({ baseURL: 'https://carplatemvp.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://carplatemvp.onrender.com/api' });
 
 export const getPlates = () => API.get('/plates');
 export const claimPlate = (data) => API.post('/plates/claim', data); // <-- this!
 export const sendMessage = (data) => API.post('/message', data);
 export const getMessages = () => API.get('/plates/messages');
 export const registerPlate = (data) => API.post('/register', data);
+export const getOwnedPlates = (userId) => API.get(`/plates/owned/${userId}`);
